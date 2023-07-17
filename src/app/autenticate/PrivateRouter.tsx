d46b61a24ef39c08e23ceda2192efccf1fa1e2d4
unknown <dasilvadeoliveirafabio@gmail.com>
2023-07-17 19:30:23 -0300

    teste de implantação 1

--- src/app/autenticate/PrivateRouter.tsx
+++ src/app/autenticate/PrivateRouter.tsx
@@ -1,35 +1,11 @@
 'use client'
-import { autozidacaoApi } from '@/APIs/authApi'
-import React,{ReactNode, useState,useEffect} from 'react'
-import {useRouter} from 'next/navigation';
-import { loginType } from '@/Types';
-import TelaDeLogin from '../TelaDeLogin/page';
-
-type childrenType={
-    children:ReactNode
+import React, { ReactNode } from 'react'
+import {SessionProvider} from 'next-auth/react'
+export default function PrivateRouter({children}:{children:ReactNode}) {
+  return (
+    <SessionProvider>
+      {children}
+    </SessionProvider>
+  )
 }
-export default function PrivateRouter({children}:childrenType) {
-  const router = useRouter()
-  const [logado, setlogado] = useState<loginType>()
-  const [loading, setloading] = useState(true)
-  async function HandleLogin() {
-      const auth:any =await autozidacaoApi()
-      setlogado(auth)
-      setloading(false)
-  }
 
-  useEffect(()=>{
-    HandleLogin()
-  },[router,loading])
-
-  if (loading) {
-    return <div>carregando... </div>
-  }else{
-    if (localStorage.getItem('jwt')) {
-      return <div>{children}</div>
-    }else{
-      router.push("/TelaDeLogin")
-      return <div></div>
-    }
-  }
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,10 @@
-import Image from 'next/image'
-import ButtomSignOut from './ButtomSignOut'
-import Link from 'next/link'
-import {useRouter} from 'next/navigation';
-import { Button } from "@mui/material";
-
+"use client"
+import {useSession} from 'next-auth/react'
 export default function Home() {
-  const router = useRouter()
-
-  const routerClick = ()=>{
-    router.push('/TelaDeElenco')
-  }
+  const {data} = useSession()
   return (
     <div>
-
-      homes
-      <ButtomSignOut/>
-      <Button onClick={routerClick}>Elencos</Button>
+      home 
     </div>
   )
 }
