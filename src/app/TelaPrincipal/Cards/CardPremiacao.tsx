3935c2b71655d8667f256575f8b120bfa5b687fe
unknown <dasilvadeoliveirafabio@gmail.com>
2023-07-21 12:52:33 -0300

    btn flutuante das premiações

--- src/app/TelaPrincipal/Cards/CardPremiacao.tsx
+++ src/app/TelaPrincipal/Cards/CardPremiacao.tsx
@@ -8,57 +8,94 @@ import BtnScroll from '../btnScroll'
 
 export default function CardPremiacao() {
   const participantes = useAppSelector(state=>state.participantesCheckedReducer.paticipantesChecked)
-  const campeoes = useAppSelector(state=>state.colocaçãoReducer.colocacao)
+  const colocacao = useAppSelector(state=>state.colocaçãoReducer.colocacao)
   const artilheiros = useAppSelector(state=>state.artilheirosReducer.artilheiros)
   const assistentes = useAppSelector(state=>state.assistentesReducer.assistente)
   const dados = useAppSelector(state=>state.golsEmpVitoriasReducer.golsEmpVitorias)
   return (
     <div className={styled.cardTorneioMain}>
+      
       <div>
-
-        <h2 style={{textAlign:"center"}}>Premiações</h2>
-        <div>
-          Campeão {campeoes?.primeiro?.nome}
-        </div>
-        <div>
-          Segundo  {campeoes?.segundo?.nome}
-        </div>
-        <div>
-          Terceiro  {campeoes?.terceiro?.nome}
-        </div>
-        <div>
-          Quarto  {campeoes?.quarto?.nome}
-        </div>
-        <h2>Artilheiros</h2>
+        <h1 className={styled.titlePremiacoes}>Premiações</h1>
         {
-          artilheiros?.primeiro.map((a, key)=>{
-            return <div key={key}>
-              artilheiro {a.jogador.nome} part {a.participante.participante.nome}
+          colocacao?.primeiro?.nome && 
+          <div>
+            <div>Campeão</div>
+            <div>
+              <div className="flex gap-x-4 ml-6 mt-1 mb-4">
+                1° <img className="h-7 w-7 flex-none rounded-full bg-gray-50" 
+                 src={colocacao.primeiro.dados.emblemaDoTime} alt="" />
+              <div className="min-w-0 flex-auto">
+                <div className="text-sm font-semibold leading-3 text-gray-900">
+                  {colocacao.primeiro.nome}
+                </div>
+                <div className="mt-1 truncate text-xs leading-2 text-gray-500">
+                  {colocacao.primeiro.dados.time}
+                </div>
+              </div>
             </div>
-          })
-        }
-              {
-          artilheiros?.segundo.map((a, key)=>{
-            return <div  key={key}>
-            vice artilheiro {a.jogador.nome} part {a.participante.participante.nome}
             </div>
-          })
+          </div>
         }
-
-      <h2>Assistentes</h2>
         {
-          assistentes?.primeiro.map((a, key)=>{
-            return <div  key={key}>
-              assistentes {a.jogador.nome} part {a.participante.participante.nome}
+          colocacao?.segundo?.nome && 
+          <div>
+            <div>Vice campeão</div>
+            <div>
+              <div className="flex gap-x-4 ml-6 mt-1 mb-4">
+                2° <img className="h-7 w-7 flex-none rounded-full bg-gray-50" 
+                 src={colocacao.segundo.dados.emblemaDoTime} alt="" />
+              <div className="min-w-0 flex-auto">
+                <div className="text-sm font-semibold leading-3 text-gray-900">
+                  {colocacao.segundo.nome}
+                </div>
+                <div className="mt-1 truncate text-xs leading-2 text-gray-500">
+                  {colocacao.segundo.dados.time}
+                </div>
+              </div>
             </div>
-          })
+            </div>
+          </div>
         }
-              {
-          assistentes?.segundo.map((a, key)=>{
-            return <div  key={key}>
-            vice assistentes {a.jogador.nome} part {a.participante.participante.nome}
+       {
+          colocacao?.terceiro?.nome && 
+          <div>
+            <div>Terceiro colocado</div>
+            <div>
+              <div className="flex gap-x-4 ml-6 mt-1 mb-4">
+                3° <img className="h-7 w-7 flex-none rounded-full bg-gray-50" 
+                 src={colocacao.terceiro.dados.emblemaDoTime} alt="" />
+              <div className="min-w-0 flex-auto">
+                <div className="text-sm font-semibold leading-3 text-gray-900">
+                  {colocacao.terceiro.nome}
+                </div>
+                <div className="mt-1 truncate text-xs leading-2 text-gray-500">
+                  {colocacao.terceiro.dados.time}
+                </div>
+              </div>
             </div>
-          })
+            </div>
+          </div>
+        }
+       {
+          colocacao?.quarto?.nome && 
+          <div>
+            <div>Quarto colocado</div>
+            <div>
+              <div className="flex gap-x-4 ml-6 mt-1 mb-4">
+                4° <img className="h-7 w-7 flex-none rounded-full bg-gray-50" 
+                 src={colocacao.quarto.dados.emblemaDoTime} alt="" />
+              <div className="min-w-0 flex-auto">
+                <div className="text-sm font-semibold leading-3 text-gray-900">
+                  {colocacao.quarto.dados.time}
+                </div>
+                <div className="mt-1 truncate text-xs leading-2 text-gray-500">
+                  {colocacao.quarto.nome}
+                </div>
+              </div>
+            </div>
+            </div>
+          </div>
         }
       </div>
       <div>
